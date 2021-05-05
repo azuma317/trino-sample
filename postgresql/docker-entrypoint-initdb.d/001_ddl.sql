9e7c6d6c9a1204b6abf21b5a487f27cc77fd49c3
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-05-05 14:42:22 +0900

    Add postgresql in docker

---
+++ postgresql/docker-entrypoint-initdb.d/001_ddl.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users
+(
+  user_id               integer UNIQUE,
+  user_name             varchar(120) NOT NULL
+);
---
+++ postgresql/docker-entrypoint-initdb.d/002_insert.sql
@@ -0,0 +1,7 @@
+insert into users(user_id, user_name) values 
+  (1, 'Danilo Hoang Bhandari'),
+  (2, 'Jacques Ace Faulkner'),
+  (3, 'Dipak Bubba Hopper'),
+  (4, 'Mihir Aji Ace'),
+  (5, 'Kabir Bayu Humphries')
+;
